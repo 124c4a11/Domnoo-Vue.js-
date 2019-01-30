1c6f426ea27adbd37fdc011e8ab5d3f3e18f0b2b
124c4a11 <124c4a11@mail.ru>
2019-01-30 20:15:50 +0300

    Header: add drawer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,16 @@
         <img :src="logo" alt="domnoo">
       </div>
       <div class="page-header__nav">
-        <button class="menu-toggle"><img :src="hamburgerIcon" alt=""></button>
+        <button
+          @click="showNavigation = !showNavigation"
+          class="menu-toggle"
+        ><img :src="hamburgerIcon" alt=""></button>
       </div>
     </div>
+    <md-drawer
+      :md-active.sync="showNavigation"
+      class="side-drawer md-right"
+    ></md-drawer>
   </header>
 </template>
 
@@ -18,6 +25,7 @@ import hamburgerIcon from '@/assets/images/menu.png'
 export default {
   data () {
     return {
+      showNavigation: false,
       logo,
       hamburgerIcon
     }

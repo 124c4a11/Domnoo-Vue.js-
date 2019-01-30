478a4a85bd3111aceaeb38107131ed66fa36e9ed
124c4a11 <124c4a11@mail.ru>
2019-01-30 18:58:41 +0300

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import '@/assets/style.css'
+
+import appHeader from '@/components/Header.vue'
 
 export default {
   name: 'app',
   components: {
+    appHeader
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="page-header">
+    <div class="container clear">
+      <div class="page-header__logo">
+        <img :src="logo" alt="domnoo">
+      </div>
+      <div class="page-header__nav">
+        <button class="menu-toggle"><img :src="hamburgerIcon" alt=""></button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import logo from '@/assets/images/logo.png'
+import hamburgerIcon from '@/assets/images/menu.png'
+
+export default {
+  data () {
+    return {
+      logo,
+      hamburgerIcon
+    }
+  }
+}
+</script>
